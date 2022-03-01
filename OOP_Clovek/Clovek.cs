4b8018f33aaa91e17cec7a317f85c1cf7ec03460
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-03-01 10:23:14 +0100

    Add project files.

---
+++ OOP_Clovek/Clovek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Clovek
+{
+    public class Clovek
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public DateTime DatumNarozeni { get; set; }
+        private int energie;
+        public int Energie { get=>energie; }
+        public int Zivot { get; set; }
+        private int dovednost;
+        public int Dovednost { get=>dovednost; }
+
+        public Clovek()
+        {
+            Jmeno = "";
+            Prijmeni = "";
+            DatumNarozeni = DateTime.Now;
+            energie = 100;
+            Zivot = 100;
+            dovednost = 0;
+        }
+
+        public void ZvysitDovednost()
+        {
+            dovednost += 20;
+            energie += 10;
+        }
+    }
+}
